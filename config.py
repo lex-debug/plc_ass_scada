209218240e681b58ea0fc61178d132675c2703c6
lex-debug <simshan@graduate.utm.my>
2023-06-24 23:30:52 +0800

    Add a new remote database config that I forgot

--- config.py
+++ config.py
@@ -3,9 +3,10 @@ import os
 class Config:
     DEBUG = False
     DEVELOPMENT = False
-    DB_HOST = os.getenv("DB_HOST", "this-is-default-host")
-    DB_USER = os.getenv("DB_USER", "this-is-default-user")
-    DB_PASS = os.getenv("DB_PASS", "this-is-default-pass")
+    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_USER = os.getenv("DB_USER", "root")
+    DB_PASS = os.getenv("DB_PASS", "sim1106")
+    DB_NAME = os.getenv("DB_NAME", "plc_ass_scada_db")
     SECRET_KEY = os.getenv("SECRET_KEY", "this-is-the-default-key")
 
 class ProductionConfig(Config):
